0c7fd97779e20b671f0dd217b6362cbd5e6cb499
Alex Nasa <alexnasa@users.noreply.huggingface.co>
2025-06-30 22:01:16 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ import subprocess
 import torch
 import gradio as gr
 import string
-import random, time, os, math   
+import random, time, math   
 
 from src.flux.generate import generate_from_test_sample, seed_everything
 from src.flux.pipeline_tools import CustomFluxPipeline, load_modulation_adapter, load_dit_lora
@@ -30,7 +30,59 @@ from eval.tools.florence_sam import ObjectDetector
 import shutil
 import yaml
 import numpy as np
-from src import environmentdata
+from huggingface_hub import snapshot_download
+import os
+
+# FLUX.1-dev
+snapshot_download(
+    repo_id="black-forest-labs/FLUX.1-dev",
+    local_dir="./FLUX.1-dev",
+    local_dir_use_symlinks=False
+)
+
+# Florence-2-large
+snapshot_download(
+    repo_id="microsoft/Florence-2-large",
+    local_dir="./Florence-2-large",
+    local_dir_use_symlinks=False
+)
+
+# CLIP ViT Large
+snapshot_download(
+    repo_id="openai/clip-vit-large-patch14",
+    local_dir="./clip-vit-large-patch14",
+    local_dir_use_symlinks=False
+)
+
+# DINO ViT-s16
+snapshot_download(
+    repo_id="facebook/dino-vits16",
+    local_dir="./dino-vits16",
+    local_dir_use_symlinks=False
+)
+
+# mPLUG Visual Question Answering
+snapshot_download(
+    repo_id="xingjianleng/mplug_visual-question-answering_coco_large_en",
+    local_dir="./mplug_visual-question-answering_coco_large_en",
+    local_dir_use_symlinks=False
+)
+
+# XVerse
+snapshot_download(
+    repo_id="ByteDance/XVerse",
+    local_dir="./XVerse",
+    local_dir_use_symlinks=False
+)
+
+
+os.environ["FLORENCE2_MODEL_PATH"]    = "./checkpoints/Florence-2-large"
+os.environ["SAM2_MODEL_PATH"]         = "./checkpoints/sam2.1_hiera_large.pt"
+os.environ["FACE_ID_MODEL_PATH"]      = "./checkpoints/model_ir_se50.pth"
+os.environ["CLIP_MODEL_PATH"]         = "./checkpoints/clip-vit-large-patch14"
+os.environ["FLUX_MODEL_PATH"]         = "./checkpoints/FLUX.1-dev"
+os.environ["DPG_VQA_MODEL_PATH"]      = "./checkpoints/mplug_visual-question-answering_coco_large_en"
+os.environ["DINO_MODEL_PATH"]         = "./checkpoints/dino-vits16"
 
 dtype = torch.bfloat16
 device = "cuda"
