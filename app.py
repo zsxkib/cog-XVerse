a874f06d4626c4991a427772f1819608b1066a6b
Alex Nasa <alexnasa@users.noreply.huggingface.co>
2025-07-01 14:02:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ run_mode = "mod_only"
 store_attn_map = False
 run_name = time.strftime("%m%d-%H%M")
 
-num_inputs = 6
+num_inputs = 2
 
 images = []
 captions = []
@@ -215,14 +215,13 @@ def generate_image(
     latent_sblora_scale_str, vae_lora_scale,
     indices,
     session_id,
-    *images_captions_faces,
+    *images,  
+    *captions, 
+    *idips_checkboxes,
 ):
     torch.cuda.empty_cache()
     num_images = 1
 
-    images = list(images_captions_faces[:num_inputs])
-    captions = list(images_captions_faces[num_inputs:2 * num_inputs])
-    idips_checkboxes = list(images_captions_faces[2 * num_inputs:3 * num_inputs])
     images = [images[i] for i in indices]
     captions = [captions[i] for i in indices]
     idips_checkboxes = [idips_checkboxes[i] for i in indices]
