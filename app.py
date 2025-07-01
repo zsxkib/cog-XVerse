41cddf440365b80e03afefc21b26f1a41d413a75
Alex Nasa <alexnasa@users.noreply.huggingface.co>
2025-07-01 01:07:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+# STEP 1: Very first thing in the file: force spawn
+import multiprocessing as mp
+mp.set_start_method("spawn", force=True)
+
 import spaces
 
 import tempfile
