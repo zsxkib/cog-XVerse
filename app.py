57d39863cbefa01511807a66b08d6db86927cbca
Alex Nasa <alexnasa@users.noreply.huggingface.co>
2025-07-01 15:00:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -39,63 +39,63 @@ import numpy as np
 from huggingface_hub import snapshot_download, hf_hub_download
 import torch
 
-# FLUX.1-dev
-snapshot_download(
-    repo_id="black-forest-labs/FLUX.1-dev",
-    local_dir="./checkpoints/FLUX.1-dev",
-    local_dir_use_symlinks=False
-)
+# # FLUX.1-dev
+# snapshot_download(
+#     repo_id="black-forest-labs/FLUX.1-dev",
+#     local_dir="/data/checkpoints/FLUX.1-dev",
+#     local_dir_use_symlinks=False
+# )
 
-# Florence-2-large
-snapshot_download(
-    repo_id="microsoft/Florence-2-large",
-    local_dir="./checkpoints/Florence-2-large",
-    local_dir_use_symlinks=False
-)
+# # Florence-2-large
+# snapshot_download(
+#     repo_id="microsoft/Florence-2-large",
+#     local_dir="/data/checkpoints/Florence-2-large",
+#     local_dir_use_symlinks=False
+# )
 
-# CLIP ViT Large
-snapshot_download(
-    repo_id="openai/clip-vit-large-patch14",
-    local_dir="./checkpoints/clip-vit-large-patch14",
-    local_dir_use_symlinks=False
-)
+# # CLIP ViT Large
+# snapshot_download(
+#     repo_id="openai/clip-vit-large-patch14",
+#     local_dir="/data/checkpoints/clip-vit-large-patch14",
+#     local_dir_use_symlinks=False
+# )
 
-# DINO ViT-s16
-snapshot_download(
-    repo_id="facebook/dino-vits16",
-    local_dir="./checkpoints/dino-vits16",
-    local_dir_use_symlinks=False
-)
+# # DINO ViT-s16
+# snapshot_download(
+#     repo_id="facebook/dino-vits16",
+#     local_dir="/data/checkpoints/dino-vits16",
+#     local_dir_use_symlinks=False
+# )
 
-# mPLUG Visual Question Answering
-snapshot_download(
-    repo_id="xingjianleng/mplug_visual-question-answering_coco_large_en",
-    local_dir="./checkpoints/mplug_visual-question-answering_coco_large_en",
-    local_dir_use_symlinks=False
-)
+# # mPLUG Visual Question Answering
+# snapshot_download(
+#     repo_id="xingjianleng/mplug_visual-question-answering_coco_large_en",
+#     local_dir="/data/checkpoints/mplug_visual-question-answering_coco_large_en",
+#     local_dir_use_symlinks=False
+# )
 
-# XVerse
-snapshot_download(
-    repo_id="ByteDance/XVerse",
-    local_dir="./checkpoints/XVerse",
-    local_dir_use_symlinks=False
-)
+# # XVerse
+# snapshot_download(
+#     repo_id="ByteDance/XVerse",
+#     local_dir="/data/checkpoints/XVerse",
+#     local_dir_use_symlinks=False
+# )
 
-hf_hub_download(
-    repo_id="facebook/sam2.1-hiera-large",
-    local_dir="./checkpoints/",
-    filename="sam2.1_hiera_large.pt",
-)
+# hf_hub_download(
+#     repo_id="facebook/sam2.1-hiera-large",
+#     local_dir="/data/checkpoints/",
+#     filename="sam2.1_hiera_large.pt",
+# )
 
 
 
-os.environ["FLORENCE2_MODEL_PATH"]    = "./checkpoints/Florence-2-large"
-os.environ["SAM2_MODEL_PATH"]         = "./checkpoints/sam2.1_hiera_large.pt"
+os.environ["FLORENCE2_MODEL_PATH"]    = "/data/checkpoints/Florence-2-large"
+os.environ["SAM2_MODEL_PATH"]         = "/data/checkpoints/sam2.1_hiera_large.pt"
 os.environ["FACE_ID_MODEL_PATH"]      = "./checkpoints/model_ir_se50.pth"
-os.environ["CLIP_MODEL_PATH"]         = "./checkpoints/clip-vit-large-patch14"
-os.environ["FLUX_MODEL_PATH"]         = "./checkpoints/FLUX.1-dev"
-os.environ["DPG_VQA_MODEL_PATH"]      = "./checkpoints/mplug_visual-question-answering_coco_large_en"
-os.environ["DINO_MODEL_PATH"]         = "./checkpoints/dino-vits16"
+os.environ["CLIP_MODEL_PATH"]         = "/data/checkpoints/clip-vit-large-patch14"
+os.environ["FLUX_MODEL_PATH"]         = "/data/checkpoints/FLUX.1-dev"
+os.environ["DPG_VQA_MODEL_PATH"]      = "/data/checkpoints/mplug_visual-question-answering_coco_large_en"
+os.environ["DINO_MODEL_PATH"]         = "/data/checkpoints/dino-vits16"
 
 dtype = torch.bfloat16
 device = "cuda"
@@ -131,7 +131,7 @@ accordions = []
 idip_checkboxes = []
 accordion_states = []
 
-ckpt_root = "./checkpoints/XVerse"
+ckpt_root = "/data/checkpoints/XVerse"
 model.clear_modulation_adapters()
 model.pipe.unload_lora_weights()
 if not os.path.exists(ckpt_root):
@@ -569,9 +569,9 @@ if __name__ == "__main__":
                 db_latent_lora_scale_str, sb_latent_lora_scale_str, vae_lora_scale_str,
                 indices_state,
                 session_state,
-                [*images],  
-                [*captions], 
-                [*idip_checkboxes],
+                *images,  
+                *captions, 
+                *idip_checkboxes,
             ], 
             outputs=output
         )
